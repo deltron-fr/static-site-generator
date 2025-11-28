1aea23d29bb25e39cf4a4c21f1c34ddbe82fc038
deltron-fr <kayceoogbonnaya2304@gmail.com>
2025-11-28 05:19:53 +0100

    added inline markdowns for images and links

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,10 +1,9 @@
 from textnode import TextType, TextNode
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     text_nodes = []
-
-    delimiter_count = 0 
 
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
@@ -14,30 +13,32 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             text_nodes.append(old_node)
 
     for text_node in text_nodes:
-        for char in text_node.text:
-            if delimiter == char:
-                delimiter_count += 1
-    
-    if delimiter_count % 2 != 0:
-        raise Exception("Invalid text, missing delimiter")
-    
-
-    for text_node in text_nodes:
         text_split = text_node.text.split(delimiter)
-    
-    for i in range(len(text_split)):
-        if i % 2 == 0:
-            text_nodes.append(TextNode(text_split[i], TextType.TEXT))
-        else:
-            text_nodes.append(TextNode(text_split[i], text_type))
+        
+        if len(text_split) % 2 != 1:
+            raise Exception("Invalid text, missing delimiter")
+        
+        for i in range(len(text_split)):
+            if i % 2 == 0:
+                if not text_split[i]:
+                    continue
+                new_nodes.append(TextNode(text_split[i], TextType.TEXT))
+            else:
+                if not text_split[i]:
+                    continue
+                new_nodes.append(TextNode(text_split[i], text_type))
 
-
-    new_nodes.extend(text_nodes)
 
     return new_nodes
 
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
 
 
 
-    
 
